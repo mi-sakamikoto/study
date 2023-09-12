c755be849c88714d0f375dcede7504b9b307ec9e
FYB <849854223@qq.com>
2023-09-12 20:07:24 +0800

    练习1-2部分,二次修改

--- src/main/work001/Bank.java
+++ src/main/work001/Bank.java
@@ -19,7 +19,7 @@ public class Bank {
 	public static Account[] add(){
 		for (int i = 0; i < users.length-1; i++){
 			//？？？？？？
-			users[i] = account.getId() + account.getPassword() + account.getName() + account.getPersonId() + account.getIdType();
+			//users[i] = account.getId() + account.getPassword() + account.getName() + account.getPersonId() + account.getIdType();
 		}
 		return users;
 	}
@@ -27,24 +27,11 @@ public class Bank {
 	 * 用户开户
 	 * @return Account对象
 	 */
-	public Account create(){
-		Scanner sc = new Scanner(System.in);
-		System.out.println("请输入账户号码:");
-		Long id = sc.nextLong();
-		Scanner sc2 = new Scanner(System.in);
-		System.out.println("请输入账户密码:");
-		String password = sc2.nextLine();
-		System.out.println("请输入确认密码:");
-		String repassword = sc2.nextLine();
-		System.out.println("请输入姓名:");
-		String name = sc2.nextLine();
-		System.out.println("请输入身份证号码:");
-		String personId = sc2.nextLine();
-		System.out.println("账户类型:");
-		String idType = sc2.nextLine();
+	public Account create(long id,String password,String repassword,String name,String personId,String idType){
+		/**/
 		if("".equals(repassword) || !repassword.equals(password)){
 			System.out.println("确认密码错误,重新开户");
-			this.create();
+			//create(id,password,repassword,name,personId,idType);
 		}else {
 			account.setId(id);
 			account.setPassword(password);
